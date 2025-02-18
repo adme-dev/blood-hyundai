42716e9ec386bc0316e3d74dc66ad03f504f946c
Paul008 <paul@adme.net.au>
2025-02-18 16:22:58 +1100

    Update DealershipReviews.vue

--- src/components/page-elements/DealershipReviews.vue
+++ src/components/page-elements/DealershipReviews.vue
@@ -3,7 +3,7 @@
       <div class="uk-container">
         <h2 class="uk-text-center uk-h1 uk-text-primary uk-text-bold uk-margin-medium-bottom">Customer <b>Reviews</b></h2>
         <div v-if="getDealershipReviews.length > 0">
-          <div class="uk-position-relative uk-visible-toggle" tabindex="-1" uk-slider="autoplay: true; autoplay-interval: 3000; center: true">
+          <div class="uk-position-relative uk-visible-toggle" tabindex="-1" uk-slider="autoplay: true; autoplay-interval: 3000">
             <ul class="uk-slider-items uk-child-width-1-1 uk-child-width-1-2@s uk-child-width-1-3@m uk-grid-small uk-grid uk-grid-match" uk-height-match="target: > li > div > .uk-card">
               <li v-for="review in positiveReviews" :key="review.time">
                 <div class="uk-card uk-padding-small uk-flex uk-flex-column">
