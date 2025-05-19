e2bb176726ec196a380503a3ee9c9c70df550b79
Paul008 <paul@adme.net.au>
2025-05-19 17:03:42 +1000

    Update tradingDetails.vue

--- src/components/page-elements/tradingDetails.vue
+++ src/components/page-elements/tradingDetails.vue
@@ -1,128 +1,127 @@
 <template>
-  <div>
+<div>
 
 
-    <div class="uk-grid-collapse uk-grid">
-      <div>
-        <div class="uk-text-medium uk-text-bold"> SALES</div>
-        <div class="uk-panel">
-          <a :href="Salesdirections" target="_blank" class="uk-link-text"> Get Directions</a>
-        </div>
-        <div class="uk-panel">
-          <a itemprop="telephone" :href="`tel:${FormatPhone(PhoneSales)}`" class="forcelink"> {{ PhoneSales }}</a>
-        </div>
-
-        <div class="uk-first-column">
-
-          <div class="uk-margin-small-top"> Sales Trading Hours </div>
-
-          <div class="uk-text-small">
-            <span class="ft-xm">Monday – Friday: </span>
-            <span v-if="Salestrading.monday[0].open">{{ GetTime(Salestrading.monday[0].open) }}</span>
-            –
-            <span v-if="Salestrading.friday[0].open">{{ GetTime(Salestrading.friday[0].close) }}</span>
+      <div class="uk-grid-collapse uk-grid">
+        <div>
+          <div class="uk-text-medium uk-text-bold"> SALES</div>
+          <div class="uk-panel">
+            <a :href="Salesdirections" target="_blank" class="uk-link-text"> Get Directions</a>
+          </div>
+          <div class="uk-panel">
+            <a itemprop="telephone" :href="`tel:${FormatPhone(PhoneSales)}`" class="forcelink"> {{PhoneSales}}</a>
           </div>
 
-          <div class="uk-text-small">
-            <span class="ft-xm">Saturday: </span>
-            <span v-if="Salestrading.saturday[0].current.value != 'open'">
-              {{ Salestrading.saturday[0].current.label }}</span>
-            <span v-else>
-              <span>{{ GetTime(Salestrading.saturday[0].open) }}</span>
-              <span v-if="Salestrading.saturday[0].close"> - {{ GetTime(Salestrading.saturday[0].close) }}</span>
-            </span>
+          <div class="uk-first-column">
+
+            <div class="uk-margin-small-top"> Sales Trading Hours </div>
+
+            <div class="uk-text-xsmall">
+              <span class="ft-xm">Monday – Friday: </span>
+                <span v-if="Salestrading.monday[0].open">{{Salestrading.monday[0].open}}</span>
+                –
+                <span v-if="Salestrading.friday[0].open">{{Salestrading.friday[0].close}}</span>
+            </div>
+
+
+            <div class="uk-text-xsmall">
+              <span class="ft-xm">Saturday: </span>
+              <span v-if="Salestrading.saturday[0].current.value != 'open'"> {{Salestrading.saturday[0].current.label}}</span>
+              <span v-else>
+                <span>{{Salestrading.saturday[0].open}}</span>
+                <span v-if="Salestrading.saturday[0].close"> - {{Salestrading.saturday[0].close}}</span>
+              </span>
+            </div>
+
+            <div class="uk-text-xsmall">
+              <span class="ft-xm">Sunday: </span>
+              <span v-if="Salestrading.sunday[0].current.value != 'open'"> {{Salestrading.sunday[0].current.label}}</span>
+              <span v-else>
+                <span>{{Salestrading.sunday[0].open}}</span>
+                <span v-if="Salestrading.sunday[0].close"> - {{Salestrading.sunday[0].close}}</span>
+              </span>
+            </div>
+
           </div>
-
-          <div class="uk-text-small">
-            <span class="ft-xm">Sunday: </span>
-            <span v-if="Salestrading.sunday[0].current.value != 'open'"> {{ Salestrading.sunday[0].current.label }}</span>
-            <span v-else>
-              <span>{{ GetTime(Salestrading.sunday[0].open) }}</span>
-              <span v-if="Salestrading.sunday[0].close"> - {{ GetTime(Salestrading.sunday[0].close) }}</span>
-            </span>
-          </div>
-
-        </div>
-      </div>
-    </div>
-
-
-
-    <div class="uk-grid-collapse uk-margin-small-top uk-grid">
-      <div>
-        <div class="uk-text-medium uk-text-bold"> SERVICE & PARTS</div>
-        <div class="uk-panel">
-          <a :href="Servicedirections" target="_blank" class="uk-link-text"> Get Directions</a>
-        </div>
-        <div class="uk-panel">
-          <a itemprop="telephone" :href="`tel:${FormatPhone(PhoneService)}`" class="forcelink"> {{ PhoneService }}</a>
-        </div>
-
-        <div class="uk-first-column">
-
-          <div class="uk-margin-small-top"> Service Trading Hours </div>
-
-          <div class="uk-text-small">
-            <span class="ft-xm">Monday – Friday: </span>
-            <span v-if="Servicetrading.monday[0].open">{{ GetTime(Servicetrading.monday[0].open) }}</span>
-            –
-            <span v-if="Servicetrading.friday[0].open">{{ GetTime(Servicetrading.friday[0].close) }}</span>
-          </div>
-
-          <div class="uk-text-small">
-            <span class="ft-xm">Saturday: </span>
-            <span v-if="Servicetrading.saturday[0].current.value != 'open'">
-              {{ Servicetrading.saturday[0].current.label }}</span>
-            <span v-else>
-              <span>{{ GetTime(Servicetrading.saturday[0].open) }}</span>
-              <span v-if="Servicetrading.saturday[0].close"> - {{ GetTime(Servicetrading.saturday[0].close) }}</span>
-            </span>
-          </div>
-
-          <div class="uk-text-small">
-            <span class="ft-xm">Sunday: </span>
-            <span v-if="Servicetrading.sunday[0].current.value != 'open'">
-              {{ Servicetrading.sunday[0].current.label }}</span>
-            <span v-else>
-              <span>{{ GetTime(Servicetrading.sunday[0].open) }}</span>
-              <span v-if="Servicetrading.sunday[0].close"> - {{ GetTime(Servicetrading.sunday[0].close) }}</span>
-            </span>
-          </div>
-
-
-          <div class="uk-margin-small-top"> Parts Trading Hours </div>
-
-          <div class="uk-text-small">
-            <span class="ft-xm">Monday – Friday: </span>
-            <span v-if="Partstrading.monday[0].open">{{ GetTime(Partstrading.monday[0].open) }}</span>
-            –
-            <span v-if="Partstrading.friday[0].open">{{ GetTime(Partstrading.friday[0].close) }}</span>
-          </div>
-
-          <div class="uk-text-small">
-            <span class="ft-xm">Saturday: </span>
-            <span v-if="Partstrading.saturday[0].current.value != 'open'">
-              {{ Partstrading.saturday[0].current.label }}</span>
-            <span v-else>
-              <span>{{ GetTime(Partstrading.saturday[0].open) }}</span>
-              <span v-if="Partstrading.saturday[0].close"> - {{ GetTime(Partstrading.saturday[0].close) }}</span>
-            </span>
-          </div>
-
-          <div class="uk-text-small">
-            <span class="ft-xm">Sunday: </span>
-            <span v-if="Partstrading.sunday[0].current.value != 'open'"> {{ Partstrading.sunday[0].current.label }}</span>
-            <span v-else>
-              <span>{{ GetTime(Partstrading.sunday[0].open) }}</span>
-              <span v-if="Partstrading.sunday[0].close"> - {{ GetTime(Partstrading.sunday[0].close) }}</span>
-            </span>
-          </div>
-
-
-
         </div>
       </div>
-    </div>
+
+
+
+      <div class="uk-grid-collapse uk-margin-small-top uk-grid">
+        <div>
+          <div class="uk-text-medium uk-text-bold"> SERVICE & PARTS</div>
+          <div class="uk-panel">
+            <a :href="Servicedirections" target="_blank" class="uk-link-text"> Get Directions</a>
+          </div>
+          <div class="uk-panel">
+            <a itemprop="telephone" :href="`tel:${FormatPhone(PhoneService)}`" class="forcelink"> {{PhoneService}}</a>
+          </div>
+
+          <div class="uk-first-column">
+
+            <div class="uk-margin-small-top"> Service Trading Hours </div>
+
+            <div class="uk-text-xsmall">
+              <span class="ft-xm">Monday – Friday: </span>
+                <span v-if="Servicetrading.monday[0].open">{{Servicetrading.monday[0].open}}</span>
+                –
+                <span v-if="Servicetrading.friday[0].open">{{Servicetrading.friday[0].close}}</span>
+            </div>
+
+
+            <div class="uk-text-xsmall">
+              <span class="ft-xm">Saturday: </span>
+              <span v-if="Servicetrading.saturday[0].current.value != 'open'"> {{Servicetrading.saturday[0].current.label}}</span>
+              <span v-else>
+                <span>{{Servicetrading.saturday[0].open}}</span>
+                <span v-if="Servicetrading.saturday[0].close"> - {{Servicetrading.saturday[0].close}}</span>
+              </span>
+            </div>
+
+            <div class="uk-text-xsmall">
+              <span class="ft-xm">Sunday: </span>
+              <span v-if="Servicetrading.sunday[0].current.value != 'open'"> {{Servicetrading.sunday[0].current.label}}</span>
+              <span v-else>
+                <span>{{Servicetrading.sunday[0].open}}</span>
+                <span v-if="Servicetrading.sunday[0].close"> - {{Servicetrading.sunday[0].close}}</span>
+              </span>
+            </div>
+
+
+            <div class="uk-margin-small-top"> Parts Trading Hours </div>
+
+            <div class="uk-text-xsmall">
+              <span class="ft-xm">Monday – Friday: </span>
+                <span v-if="Partstrading.monday[0].open">{{Partstrading.monday[0].open}}</span>
+                –
+                <span v-if="Partstrading.friday[0].open">{{Partstrading.friday[0].close}}</span>
+            </div>
+
+
+            <div class="uk-text-xsmall">
+              <span class="ft-xm">Saturday: </span>
+              <span v-if="Partstrading.saturday[0].current.value != 'open'"> {{Partstrading.saturday[0].current.label}}</span>
+              <span v-else>
+                <span>{{Partstrading.saturday[0].open}}</span>
+                <span v-if="Partstrading.saturday[0].close"> - {{Partstrading.saturday[0].close}}</span>
+              </span>
+            </div>
+
+            <div class="uk-text-xsmall">
+              <span class="ft-xm">Sunday: </span>
+              <span v-if="Partstrading.sunday[0].current.value != 'open'"> {{Partstrading.sunday[0].current.label}}</span>
+              <span v-else>
+                <span>{{Partstrading.sunday[0].open}}</span>
+                <span v-if="Partstrading.sunday[0].close"> - {{Partstrading.sunday[0].close}}</span>
+              </span>
+            </div>
+
+
+
+          </div>
+        </div>
+      </div>
 
 
 
@@ -131,40 +130,64 @@
 
 
 
-  </div>
+</div>
 </template>
 <script>
 export default {
   name: "contact-details",
   props: ["classes"],
-  data: function () {
-    return {
-      siteName: this.$store.state.site.name,
-      description: this.$store.state.site.description,
-      showroom_address: this.$store.state.site.showroom_address,
-      SalesAddress: this.$store.state.site.departments.sales.address,
-      PhoneSales: this.$store.state.site.departments.sales.phone,
-      Salestrading: this.$store.state.site.departments.sales.trading,
-      Salesdirections: this.$store.state.site.departments.sales.map_directions,
-
-      ///converted to new car sales
-      // UsedSalesAddress: this.$store.state.site.departments.sales.address,
-      // UsedPhoneSales: this.$store.state.site.departments.sales.phone,
-      // UsedSalestrading: this.$store.state.site.departments.sales.trading,
-      // UsedSalesdirections: this.$store.state.site.departments.sales.map_directions,
-
-      ServiceAddress: this.$store.state.site.departments.service.address,
-      PhoneService: this.$store.state.site.departments.service.phone,
-      Servicetrading: this.$store.state.site.departments.service.trading,
-      Servicedirections: this.$store.state.site.departments.service.map_directions,
-
-      PartsAddress: this.$store.state.site.departments.parts.address,
-      PhoneParts: this.$store.state.site.departments.parts.phone,
-      Partstrading: this.$store.state.site.departments.parts.trading,
-      Partsdirections: this.$store.state.site.departments.parts.map_directions,
-
-      lmct: this.$store.state.site.lmct,
-    };
+  computed: {
+    site() {
+      return this.$store.state.site;
+    },
+    siteName() {
+      return this.site.name;
+    },
+    description() {
+      return this.site.description;
+    },
+    showroom_address() {
+      return this.site.showroom_address;
+    },
+    SalesAddress() {
+      return this.site.departments.sales.address;
+    },
+    PhoneSales() {
+      return this.site.departments.sales.phone;
+    },
+    Salestrading() {
+      return this.site.departments.sales.trading;
+    },
+    Salesdirections() {
+      return this.site.departments.sales.map_directions;
+    },
+    ServiceAddress() {
+      return this.site.departments.service.address;
+    },
+    PhoneService() {
+      return this.site.departments.service.phone;
+    },
+    Servicetrading() {
+      return this.site.departments.service.trading;
+    },
+    Servicedirections() {
+      return this.site.departments.service.map_directions;
+    },
+    PartsAddress() {
+      return this.site.departments.parts.address;
+    },
+    PhoneParts() {
+      return this.site.departments.parts.phone;
+    },
+    Partstrading() {
+      return this.site.departments.parts.trading;
+    },
+    Partsdirections() {
+      return this.site.departments.parts.map_directions;
+    },
+    lmct() {
+      return this.site.lmct;
+    }
   },
   methods: {
     FormatPhone(phone) {
@@ -176,8 +199,10 @@ export default {
     },
   }
 };
+
 </script>
 <style>
-.ft-xm {
+.ft-xm{
   color: rgb(255 255 255 / 67%);
-}</style>
+}
+</style>
